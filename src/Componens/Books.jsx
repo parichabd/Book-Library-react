272a6225c700a99e070325c0fb80b57061c2a370
Paricherh Abedzadeh <Parichehrabedzadeh@gmail.com>
2025-10-23 11:05:13 +0330

    fix : bug

--- src/Componens/Books.jsx
+++ src/Componens/Books.jsx
@@ -1,15 +1,28 @@
+// Books.jsx
+import { useState } from "react";
 import { books } from "../constants/mockData";
 import BookList from "./BookList";
-console.log(books);
+
 function Books() {
+  const [liked, setLiked] = useState([]);
+
+  const LikeHandler = (isLiked, book) => {
+    if (isLiked) {
+      const newLikeList = liked.filter((i) => i.id !== book.id);
+      setLiked(newLikeList);
+    } else {
+      setLiked((prev) => [...prev, book]);
+    }
+  };
+
   return (
     <>
       <div>
         {books.map((book) => (
-          <BookList key={book.id} data={book} />
+          <BookList key={book.id} data={book} LikeHandler={LikeHandler} />
         ))}
       </div>
-      <div></div>
+      {!!liked.length && <div>Liked Books: {liked.length}</div>}
     </>
   );
 }
